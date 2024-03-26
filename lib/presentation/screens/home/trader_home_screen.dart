efa41c263a3969fff446cbd82d26d0b469fe4972
Dawood Khatri <dawoodkhatri18@gmail.com>
2024-03-26 14:57:49 +0530

    Integrated google map for trader

--- lib/presentation/screens/home/trader_home_screen.dart
+++ lib/presentation/screens/home/trader_home_screen.dart
@@ -2,9 +2,12 @@ import 'dart:async';
 
 import 'package:cargo_linker/logic/cubits/auth_cubit/auth_cubit.dart';
 import 'package:cargo_linker/logic/cubits/auth_cubit/auth_state.dart';
+import 'package:cargo_linker/logic/cubits/trader_container_search_cubit/trader_container_search_cubit.dart';
+import 'package:cargo_linker/logic/cubits/trader_container_search_cubit/trader_container_search_state.dart';
 import 'package:cargo_linker/presentation/screens/splash/splash_screen.dart';
 import 'package:cargo_linker/presentation/widgets/primary_button.dart';
 import 'package:cargo_linker/presentation/widgets/primary_text_field.dart';
+import 'package:cargo_linker/presentation/widgets/spacing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -21,7 +24,7 @@ class TraderHomeScreen extends StatefulWidget {
 class _TraderHomeScreenState extends State<TraderHomeScreen> {
   final Completer<GoogleMapController> _map_controller =
       Completer<GoogleMapController>();
-  final DropController = TextEditingController();
+  final dropLocationController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -53,16 +56,14 @@ class _TraderHomeScreenState extends State<TraderHomeScreen> {
               )),
             ],
           ),
-          
-          body:
-           Column(
-             children: [
-              // Here add input fields
+          body: Column(
+            children: [
               Padding(
-                padding: EdgeInsets.all(16),
-                child: 
-                PrimaryTextField(
-                      controller: DropController,
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    PrimaryTextField(
+                      controller: dropLocationController,
                       labelText: 'Drop Location',
                       validator: (value) {
                         if (value!.trim().isEmpty) {
@@ -70,32 +71,50 @@ class _TraderHomeScreenState extends State<TraderHomeScreen> {
                         }
                         return null;
                       },
-                    ),),
-              Padding(
-                padding: EdgeInsets.all(16),
-                child:
-                PrimaryButton(
-                      child: Text("Search"),
-                      onPressed: () {
-                        
-                      }),),      
-
-               Expanded(
-                 child: GoogleMap(
-                    mapType: MapType.normal,
-                    initialCameraPosition: const CameraPosition(
-                      target: LatLng(18.9681556, 72.8313206),
-                      zoom: 13,
                     ),
-                    markers: {
-                      const Marker(
-                        markerId: MarkerId("saboosiddik"),
-                        position: LatLng(18.9681556, 72.8313206),
-                      )
-                    }),
-               ),
-             ],
-           )),
+                    const Spacing(
+                      multiply: 2,
+                    ),
+                    PrimaryButton(
+                        child: const Text("Search"),
+                        onPressed: () {
+                          BlocProvider.of<TraderContainerSearchCubit>(context)
+                              .searchPickupLocations(
+                                  dropLocationController.text);
+                        }),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: BlocBuilder<TraderContainerSearchCubit,
+                    TraderContainerSearchState>(
+                  builder: (context, state) {
+                    if (state is TraderContainerSearchLoadingState) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    if (state is TraderContainerSearchedState) {
+                      return GoogleMap(
+                          mapType: MapType.normal,
+                          initialCameraPosition:  CameraPosition(
+                            target: LatLng(state.pickupLocations[0].lat, state.pickupLocations[0].long),
+                            zoom: 13,
+                          ),
+                          markers: state.pickupLocations
+                              .map((location) => Marker(
+                                    markerId: MarkerId(location.containerId),
+                                    position:
+                                        LatLng(location.lat, location.long),
+                                  ))
+                              .toSet());
+                    }
+
+                    return const Spacing();
+                  },
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
